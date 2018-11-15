4fd8e6b8886a54339118a1fb70e89b80593b9477
小青年 <lizhiqiangbj@126.com>
2018-11-15 18:36:37 +0800

    1.0版本

---
+++ WXDuckShooter/bin/js/UILogic/GameStartUILogic.js
@@ -0,0 +1,40 @@
+/**
+ * 开始游戏界面
+ */
+
+var GameStartUiLogic = (function (_super) {
+
+    function GameStartUiLogic() {
+        GameStartUiLogic.super(this);
+    }
+    Laya.class(GameStartUiLogic, "GameStartUiLogic", _super);
+    _proto = GameStartUiLogic.prototype;
+     
+
+    _proto.onInit = function () {
+        this.width = Laya.stage.width;
+        this.height = Laya.stage.height;
+        
+
+        this.btn_start.on(Laya.Event.CLICK,this,this._startClickEvent);
+        wxGame.getInstance().showClubBtn(true);
+    }
+    
+    _proto.onDestroy = function () {
+        // MusicManager.getInstance().stopMusic();
+    }
+
+    _proto._startClickEvent = function(){
+        wxGame.getInstance().showClubBtn(false);
+        wxGame.getInstance().createVideoAD();
+
+        UIManager.getInstance().closeUI("GameStartUI");
+        UIManager.getInstance().showUI("GameUI");
+
+        SceneManager.getInstance().currentScene.startGame();
+    }
+  
+
+
+    return GameStartUiLogic;
+})(GameStartUI);
