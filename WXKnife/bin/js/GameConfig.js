ffac1b7b138fc7a8946ca957239c0e5145d30c38
小青年 <lizhiqiangbj@126.com>
2018-11-25 16:26:32 +0800

    飞刀上线

---
+++ WXKnife/bin/js/GameConfig.js
@@ -0,0 +1,21 @@
+var SoundManager = laya.media.SoundManager;
+var LocalStorage = laya.net.LocalStorage;
+var Point = laya.maths.Point;
+var Sprite = laya.display.Sprite;
+var Browser = Laya.Browser;
+
+var GameConfig = {
+    
+    //游戏宽 高
+    GameWidth : 480,
+    GameHeight : 854,
+
+    //游戏速度
+    speed : 8
+    
+};
+
+/**是否显示Log */
+var GameLogVisible = true;
+/**显示范围 */
+// var ShowRang = false;
