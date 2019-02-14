9a38e76b9d03cb865cc9bbcabe14385fe6cbd41a
小青年 <lizhiqiangbj@126.com>
2019-02-14 09:57:47 +0800

    三角形提审

---
+++ WXTriangle/src/Data/LevelData.js
@@ -0,0 +1,47 @@
+ /**关卡数据 */
+var LevelData = [
+    {
+        level:1,
+        name:"",
+        score:0,
+        speed:1.5,
+    },
+    {
+        level:2,
+        name:"",
+        score:50,
+        speed:2,
+    },
+    {
+        level:3,
+        name:"",
+        score:100,
+        speed:2.5
+    },
+    {
+        level:4,
+        name:"",
+        score:250,
+        speed:3
+    },
+    {
+        level:5,
+        name:"",
+        score:350,
+        speed:3.5
+    },
+    {
+        level:6,
+        name:"",
+        score:500,
+        speed:4
+    },
+    {
+        level:7,
+        name:"",
+        score:700,
+        speed:5
+    },
+    
+    
+];
